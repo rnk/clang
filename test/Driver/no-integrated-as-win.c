454852862463447c16237863e9f130887364ead3
Reid Kleckner <rnk@google.com>
2013-06-18 09:32:43 -0400

    Testcase

--- test/Driver/no-integrated-as-win.c
+++ test/Driver/no-integrated-as-win.c
@@ -1,3 +1,9 @@
 // RUN: %clang -target x86_64-pc-win32 -### -no-integrated-as %s -c 2>&1 | FileCheck %s
-
 // CHECK: there is no external assembler we can use on windows
+
+// But there is for mingw.  The source file should only be mentioned once for
+// the compile step.
+// RUN: %clang -target i686-pc-mingw32 -### -no-integrated-as %s -c 2>&1 | FileCheck -check-prefix=MINGW %s
+// MINGW: "-cc1"
+// MINGW: no-integrated-as-win.c
+// MINGW-NOT: no-integrated-as-win.c
